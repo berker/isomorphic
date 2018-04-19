2eba602ec0a19656ca14103b18296d6499a852cc
berker.narol <berkernarol@gmail.com>
2018-04-19 17:07:46 +0300

    Use server state on client

--- client/src/client.js
+++ client/src/client.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-const username = 'berker';
+const username = window.SERVER_STATE ? window.SERVER_STATE.username : 'berker';
 ReactDOM.render(
     <App name={username} />,
     document.getElementById('root')
